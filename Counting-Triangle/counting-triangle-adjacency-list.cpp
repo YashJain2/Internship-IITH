501386d056d8d447b45a539596724ffab3db47d0
Yash <yjain8958372013@gmail.com>
2022-04-01 10:59:35 +0530

    Research Papers

--- Counting-Triangle/counting-triangle-adjacency-list.cpp
+++ Counting-Triangle/counting-triangle-adjacency-list.cpp
@@ -3,12 +3,10 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-using namespace std::chrono;
 int countTriangle=0;
 
 void displayAdjList(list<int> adj_list[], int v) {
-    vector <int> a;
-    vector <int> b;
+    vector <int> a;    vector <int> b;
   for(int i = 0; i<v; i++) {
      cout << i << "---> ";
      list<int> :: iterator it;
@@ -20,22 +18,9 @@ void displayAdjList(list<int> adj_list[], int v) {
      for(it = adj_list[i].begin(); it != adj_list[i].end(); ++it) {
          //stores the list of neighbours in *it pointer
         cout << *it << "  ";
-        
         list<int> :: iterator temp;
-        for(temp = adj_list[*it].begin(); temp != adj_list[*it].end();temp++){
+        for(temp = adj_list[*it].begin(); temp != adj_list[*it].end();temp++)
             b.push_back(*temp);
-        }
-    
-        // for(int x=0;x<a.size();x++){
-        //     cout<<a[x]<<' ';
-        // }
-        // cout<<endl;
-
-        // for(int x=0;x<b.size();x++){
-        //     cout<<b[x]<<' ';
-        // }
-        // cout<<endl;
-
         for(int ii=0;ii<b.size();ii++){
             if(b[ii]==i){
                 continue;
@@ -47,8 +32,6 @@ void displayAdjList(list<int> adj_list[], int v) {
                     }
                     else{
                         if(a[jj]==b[ii]){
-                            // cout<<endl;
-                            // cout<<i<<"\t"<<*it<<"\t"<<b[ii]<<"\t"<<a[jj]<<endl;
                             countTriangle++;
                         }
                     }
@@ -61,16 +44,13 @@ void displayAdjList(list<int> adj_list[], int v) {
    }
    cout<<"No of triangles in the given adjaceny list: "<<countTriangle/6;
 }
-
 void add_edge(list<int> adj_list[], int u, int v) {   
 //add v into the list u, and u into list v
    adj_list[u].push_back(v);
    adj_list[v].push_back(u);
 }
 
-
 int main(){
-
     int v = 6;      
     //there are 6 vertices in the graph
     //create an array of lists whose size is 6
@@ -85,6 +65,5 @@ int main(){
     add_edge(adj_list, 5, 3);
     add_edge(adj_list, 5, 4);
     displayAdjList(adj_list, v);
-
     return 0;
 }
